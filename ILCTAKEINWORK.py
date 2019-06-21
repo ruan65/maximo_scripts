6fb1b687d4e6dbfad54e22c369f31f0bf1336c79
a <ruan65@gmail.com>
2019-06-21 10:51:46 +0300

    fix

--- ILCTAKEINWORK.py
+++ ILCTAKEINWORK.py
@@ -8,10 +8,11 @@ val = request.getQueryParam('VAL')
 
 valSet = mxServer.getMboSet('WORKORDER', userInfo)
 valSet.setWhere('WONUM='+val)
-rltkt = mbo.getMboSet('RELATEDTICKET').getMbo(0)
-sr = rltkt.getMboSet('RELATEDRECTK').getMbo(0)
+
 wo = valSet.getMbo(0)
 wo.changeStatus(u'В РАБОТЕ', MXServer.getMXServer().getDate(), u'Принято в работу',4L)
+rltkt = wo.getMboSet('RELATEDTICKET').getMbo(0)
+sr = rltkt.getMboSet('RELATEDRECTK').getMbo(0)
 if sr != None:
  sr.changeStatus(u'В РЕМОНТЕ', MXServer.getMXServer().getDate(), u'Принято в работу',4L)
 valSet.save()
